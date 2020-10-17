9c84029645cb366281c542c7f7b3efd50d64e130
Victtor Freitas <victtorfreitas95@gmail.com>
2020-10-16 23:25:46 -0300

    Refes #1 Realoca arquivo testaCondicoes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -107,18 +107,3 @@ fun testaLacos() {
     }
 }
 
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0.0) {
-        println("conta é positiva")
-    } else if (saldo == 0.0) {
-        println("conta é neutra")
-    } else {
-        println("conta é negativa")
-    }
-
-    when {
-        saldo > 0.0 -> println("conta é positiva")
-        saldo == 0.0 -> println("conta é neutra")
-        else -> println("conta é negativa")
-    }
-}
