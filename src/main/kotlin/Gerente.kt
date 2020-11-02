32a9202ebbd7a03cf61f7379e8c948c254346665
Victtor Freitas <victtorfreitas95@gmail.com>
2020-11-02 13:48:06 -0300

    Refes #2 Implementa Gerente

---
+++ src/main/kotlin/Gerente.kt
@@ -0,0 +1,14 @@
+class Gerente(
+    val nome: String,
+    val cpf: String,
+    val salario: Double,
+    val senha: Int
+) {
+    fun bonificacao(): Double {
+        return salario * 0.2
+    }
+
+    fun autentica(senha: Int): Boolean {
+        return this.senha == senha
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,17 @@
 fun main() {
-    println("Bem vindo ao Bytebank")
+    println("Bem vindo ao ParkBank")
 
-    val funVicttor = Funcionario("Victtor", "027.297.121-94", 1000.0, 3)
+    val funVicttor = Funcionario("Victtor", "027.322.121-94", 1000.0)
     println("Nome: ${funVicttor.nome}")
     println("CPF: ${funVicttor.cpf}")
     println("Salario: ${funVicttor.salario}")
     println("Bonificação: ${funVicttor.bonificacao()}")
+
+    val gerenteFabia = Gerente("Fabia", "125.297.121-94", 2000.0, 123)
+    println("Nome: ${gerenteFabia.nome}")
+    println("CPF: ${gerenteFabia.cpf}")
+    println("Salario: ${gerenteFabia.salario}")
+    println("Bonificação: ${gerenteFabia.bonificacao()}")
+
+    println("Autenticado: ${if (gerenteFabia.autentica(1233)) "Sim" else "Não"}")
 }
