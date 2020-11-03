5295c05c58da448313909639365217fcccca9e10
Victtor Freitas <victtorfreitas95@gmail.com>
2020-11-02 22:34:47 -0300

    Refes #3 Implementa calculadora bonificacao

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,4 +14,19 @@ fun main() {
     println("Bonificação: ${gerenteFabia.bonificacao}")
 
     println("Autenticado: ${if (gerenteFabia.autentica(1233)) "Sim" else "Não"}")
+
+    val juniorDiretor = Diretor("Júnior", "000.111.121-94", 4000.0, 123,450.0)
+    println("Nome: ${juniorDiretor.nome}")
+    println("CPF: ${juniorDiretor.cpf}")
+    println("Salario: ${juniorDiretor.salario}")
+    println("Bonificação: ${juniorDiretor.bonificacao}")
+
+    val calculadora = CalculadoraBonificacao()
+
+    calculadora.registra(funVicttor)
+    calculadora.registra(gerenteFabia)
+    calculadora.registra(juniorDiretor)
+
+    println("O valor total pago de bonificação será: ${calculadora.total}")
+
 }
